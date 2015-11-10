ca974c8ec73d6028e97f8524c838a5641a529da5
pieterprovoost <pieterprovoost@gmail.com>
2015-11-10 21:18:17 +0100

    more tests

--- test/test.js
+++ test/test.js
@@ -4,6 +4,11 @@ var geo = require("../src/geo.js");
 
 test('parseDms', function(t) {
 	var almostEqual = fuzzy(t, 0.0001);
-    almostEqual(geo.parseDms("51°28'38''N -101°16′56″ w"), [-101.2822, 51.4772])
+
+    almostEqual(geo.parseDms("51°28'38''N -101°16'56''W"), [-101.2822, 51.4772])
+    almostEqual(geo.parseDms("51°28'38\"N -101°16'56\"W"), [-101.2822, 51.4772])
+    almostEqual(geo.parseDms("51°28`38''N -101°16′56″W"), [-101.2822, 51.4772])
+    almostEqual(geo.parseDms("51° 28' 38'' N -101° 16' 56'' W"), [-101.2822, 51.4772])
+
     t.end();
 })
