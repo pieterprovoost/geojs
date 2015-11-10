838d55bab94c3a7939c03b2cc79fda4fef9a58fb
pieterprovoost <pieterprovoost@gmail.com>
2015-11-10 21:50:53 +0100

    extra tests and multiple fixes

--- test/test.js
+++ test/test.js
@@ -5,10 +5,14 @@ var geo = require("../src/geo.js");
 test('parseDms', function(t) {
 	var almostEqual = fuzzy(t, 0.0001);
 
-    almostEqual(geo.parseDms("51°28'38''N -101°16'56''W"), [-101.2822, 51.4772])
-    almostEqual(geo.parseDms("51°28'38\"N -101°16'56\"W"), [-101.2822, 51.4772])
-    almostEqual(geo.parseDms("51°28`38''N -101°16′56″W"), [-101.2822, 51.4772])
-    almostEqual(geo.parseDms("51° 28' 38'' N -101° 16' 56'' W"), [-101.2822, 51.4772])
+    almostEqual(geo.parseDms("51°28'38''N 101°16'56''W"), [-101.2822, 51.4772])
+    almostEqual(geo.parseDms("51°28'38\"N 101°16'56\"W"), [-101.2822, 51.4772])
+    almostEqual(geo.parseDms("51°28`38''N 101°16′56″W"), [-101.2822, 51.4772])
+    almostEqual(geo.parseDms("51° 28' 38'' N 101° 16' 56'' W"), [-101.2822, 51.4772])
+    almostEqual(geo.parseDms("51 ° 28 ' 38 '' N 101 ° 16 ' 56 '' W"), [-101.2822, 51.4772])
+    almostEqual(geo.parseDms("51°28'38''N -101°16'56''E"), [-101.2822, 51.4772])
+    almostEqual(geo.parseDms("51° N 101° W"), [-101.0000, 51.0000])
+    almostEqual(geo.parseDms("51° N"), [null, 51.0000])
 
     t.end();
 })
