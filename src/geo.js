0667106009e2415faa5ce8e2165504465be79183
pieterprovoost <pieterprovoost@gmail.com>
2015-11-21 17:26:23 +0100

    convex hull

--- src/geo.js
+++ src/geo.js
@@ -35,6 +35,34 @@ var parseDms = function(input) {
 	return result;
 };	
 
+var cross = function cross(o, a, b) {
+	return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0]);
+};
+
+var convexHull = function(points) {
+	points.sort(function(a, b) {
+		return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0];
+	});
+	var lower = [];
+	for (var i = 0; i < points.length; i++) {
+		while (lower.length >= 2 && cross(lower[lower.length - 2], lower[lower.length - 1], points[i]) <= 0) {
+			lower.pop();
+		}
+		lower.push(points[i]);
+	}
+	var upper = [];
+	for (var i = points.length - 1; i >= 0; i--) {
+		while (upper.length >= 2 && cross(upper[upper.length - 2], upper[upper.length - 1], points[i]) <= 0) {
+			upper.pop();
+		}
+		upper.push(points[i]);
+	}
+	upper.pop();
+	lower.pop();
+	return lower.concat(upper);
+};
+
 module.exports = {
-	parseDms: parseDms
+	parseDms: parseDms,
+	convexHull: convexHull
 };
--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ var geo = require("../src/geo.js");
 
 test('parseDms', function(t) {
 	var almostEqual = fuzzy(t, 0.0001);
-
     almostEqual(geo.parseDms("51°28'38''N 101°16'56''W"), [-101.2822, 51.4772])
     almostEqual(geo.parseDms("51°28'38\"N 101°16'56\"W"), [-101.2822, 51.4772])
     almostEqual(geo.parseDms("51°28`38''N 101°16′56″W"), [-101.2822, 51.4772])
@@ -14,6 +13,12 @@ test('parseDms', function(t) {
     almostEqual(geo.parseDms("51° N 101° W"), [-101.0000, 51.0000])
     almostEqual(geo.parseDms("51° N"), [null, 51.0000])
     almostEqual(geo.parseDms("12° N 109° 58’ 37” W"), [-109.9769, 12.0000])
-
     t.end();
 })
+
+test('convexHull', function(t) {
+    var points = [[30, 20], [45, 40], [10, 40], [30, 20], [15, 5], [40, 10], [10, 20], [5, 10], [15, 5]];
+    var hull = geo.convexHull(points);
+    t.equal(hull.length, 5)
+    t.end();
+});
